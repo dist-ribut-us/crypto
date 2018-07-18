9628c42cab0b36aeb670fdd62c9e7f471139967c
Adam Colton <adamcolton@gmail.com>
2018-07-18 12:44:27 -0400

    Added test related to onion routing

--- unmac_test.go
+++ unmac_test.go
@@ -11,14 +11,33 @@ func TestUnmacd(t *testing.T) {
 	shared := RandomSymmetric()
 	assert.NotNil(t, shared)
 
-	nonce := &Nonce{}
-	nonce[0] = 1
+	nonce := RandomNonce()
 
 	msg := make([]byte, 100)
 	rand.Read(msg)
 
 	cipher := shared.UnmacdSeal(msg, nonce)
 	plain := shared.UnmacdOpen(cipher, nonce)
+	if !bytes.Equal(msg, plain) {
+		t.Error(msg)
+		t.Error(plain)
+		t.Error("Message does not match")
+	}
+}
+
+func TestReversed(t *testing.T) {
+	shared := RandomSymmetric()
+	assert.NotNil(t, shared)
+
+	nonce := RandomNonce()
+
+	msg := make([]byte, 100)
+	rand.Read(msg)
+
+	// UnmacdOpen and UnmacdSeal are actually arbitrary. UnmacdOpen can be used
+	// as seal if UnmacdSeal is used as open
+	cipher := shared.UnmacdOpen(msg, nonce)
+	plain := shared.UnmacdSeal(cipher, nonce)
 	if !bytes.Equal(msg, plain) {
 		t.Error(msg)
 		t.Error(plain)
